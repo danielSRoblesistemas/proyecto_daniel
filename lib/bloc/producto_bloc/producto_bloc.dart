8bdfd4e7ca96a81997b85e3c050340e33f1e5a41
Daniel <daniel.silva@roblesistemas.com.ar>
2022-09-09 16:29:35 -0300

    implementacion_bloc_en _main_y_vista

--- lib/bloc/producto_bloc/producto_bloc.dart
+++ lib/bloc/producto_bloc/producto_bloc.dart
@@ -7,55 +7,43 @@ part 'producto_event.dart';
 part 'producto_state.dart';
 
 class ProductoBloc extends Bloc<ProductoEvent, ProductoState> {
-  ProductoBloc() : super( ProductoState()) {
-
-
+  ProductoBloc() : super(ProductoState()) {
     on<OnNuevoProducto>(_onNuevoProducto);
     on<OnModificarProducto>(_onModificarProducto);
-    // on<OnValidarProducto>();
+    // on<OnValidarProducto>(_onValidarProducto);
     // on<OnGuardarProducto>();
     // on<OnActualizaProducto>();
-    // on<OnObtenerlstProducto>();
+    on<OnObtenerlstProducto>(_onObtenerlstProducto);
   }
 
-    Future<void> _onNuevoProducto(  
-      OnNuevoProducto event, Emitter emit) async {
-    emit(state.copyWith(
+  Future<void> _onNuevoProducto(OnNuevoProducto event, Emitter emit) async {
+    emit(
+      state.copyWith(isWorking: true, producto: ProductoModel(), error: '', accion: Environment.blocOnNuevoProducto),
+    );
+  }
+
+  Future<void> _onModificarProducto(OnModificarProducto event, Emitter emit) async {
+    try {
+      emit(state.copyWith(isWorking: true, error: '', campoError: '', accion: Environment.blocOnModificarProducto));
+    } catch (e) {
+      emit(state.copyWith(isWorking: false, error: e.toString(), accion: Environment.blocOnModificarProducto));
+    }
+  }
+
+  Future<void> _onObtenerlstProducto(OnObtenerlstProducto event, Emitter emit) async {
+    try {
+      emit(state.copyWith(
         isWorking: true,
-        producto:  ProductoModel(),
         error: '',
-        accion: Environment.blocOnNuevoProducto),);
-        }
+        accion: Environment.blocOnObtenerlstProducto,
+      ));
 
-    Future<void> _onModificarProducto(
-      OnModificarProducto event, Emitter emit) async{
-      try {
-        // if (errorCredenciales.isNotEmpty) {
-        //         emit(state.copyWith(
-        //           isWorking: false,  s
-        //           // error: errorCredenciales,
-        //           accion: Environment.blocOnModificarProducto));
-        //       return;
-        // }
-          emit(state.copyWith(
-          isWorking: true,
-          error: '',
-          campoError: '',
-          accion: Environment.blocOnModificarProducto));
+      String error = '';
 
-      }catch (e){
-          emit(state.copyWith(
-          isWorking: false,
-          error: e.toString(),
-          accion: Environment.blocOnModificarProducto));
-      }
-      }
+      emit(state.producto.obtenerListaEstatica());
+
+    } catch (e) {
       
-
-
-
+    }
+  }
 }
-
-
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:proyecto_daniel/bloc/producto_bloc/producto_bloc.dart';
 import 'package:proyecto_daniel/views/home_view.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
@@ -7,21 +9,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      builder: (context, child) => ResponsiveWrapper.builder(
-              child,
-              maxWidth: 1200,
-              minWidth: 480,
-              defaultScale: true,
-              breakpoints: const [
-                ResponsiveBreakpoint.autoScale(260, name: PHONE),
-                ResponsiveBreakpoint.autoScale(500, name: MOBILE),
-                ResponsiveBreakpoint.autoScale(780, name: TABLET),
-                ResponsiveBreakpoint.autoScale(1200, name: DESKTOP),
-              ],
-            ),
-      home: const HomeView()            
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: ((context) => ProductoBloc()))
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        builder: (context, child) => ResponsiveWrapper.builder(
+                child,
+                maxWidth: 1200,
+                minWidth: 480,
+                defaultScale: true,
+                breakpoints: const [
+                  ResponsiveBreakpoint.autoScale(260, name: PHONE),
+                  ResponsiveBreakpoint.autoScale(500, name: MOBILE),
+                  ResponsiveBreakpoint.autoScale(780, name: TABLET),
+                  ResponsiveBreakpoint.autoScale(1200, name: DESKTOP),
+                ],
+              ),
+        home: const HomeView()            
+      ),
     );
   }
 }
